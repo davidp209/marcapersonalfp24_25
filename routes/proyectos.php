683e92dc7b1342f273237b123ec4c1047595bd5c
David Paredes <1918862@alu.murciaeduca.es>
2024-11-26 11:36:28 +0100

    Pruebas hechas con el Middleware

--- routes/proyectos.php
+++ routes/proyectos.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('proyectos', [ProyectosController::class, 'getIndex']);
 
 Route::get('proyectos/show/{id}', [ProyectosController::class, 'getShow'])
+->middleware(['id_mayor_de_10'.':9'])
 ->where('id', '[0-9]+');
 
 Route::get('proyectos/create', [ProyectosController::class, 'getCreate']);
